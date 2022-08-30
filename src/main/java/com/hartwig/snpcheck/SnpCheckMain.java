d254465b9c12f77dc0e2c43ebf6cc856ec95d06f
Ned Leitch <ned.leitch@gmail.com>
2022-08-30 14:59:40 -0400

    DEV-2868 Remove credentials

--- src/main/java/com/hartwig/snpcheck/SnpCheckMain.java
+++ src/main/java/com/hartwig/snpcheck/SnpCheckMain.java
@@ -1,13 +1,11 @@
 package com.hartwig.snpcheck;
 
-import java.io.FileInputStream;
 import java.util.concurrent.Callable;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.StorageOptions;
@@ -26,26 +24,21 @@ public class SnpCheckMain implements Callable<Integer> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SnpCheckMain.class);
 
-    @CommandLine.Option(names = { "--snpcheck_private_key" },
-                        defaultValue = "/snpcheck_secrets/service_account.json",
-                        description = "Path to private key for the snpcheck service account")
-    private String snpcheckPrivateKeyPath;
-    @CommandLine.Option(names = { "--database_private_key" },
-                        defaultValue = "/database_secrets/service_account.json",
-                        description = "Path to private key for the database service account")
-    private String databasePrivateKeyPath;
     @CommandLine.Option(names = { "--api_url" },
                         required = true,
                         description = "URL from which to connect to the API")
     private String apiUrl;
+
     @CommandLine.Option(names = { "--snpcheck_bucket" },
                         defaultValue = "hmf-snpcheck",
                         description = "Bucket in which the snpcheck vcfs are uploaded")
     private String snpcheckBucketName;
+
     @CommandLine.Option(names = { "--project" },
                         required = true,
                         description = "Project in which the snpcheck is running")
     private String project;
+
     @CommandLine.Option(names = { "--passthru" },
             defaultValue = "false",
             description = "Mark all events as validated without actually validating against the snpcheck vcf.")
@@ -63,30 +56,22 @@ public class SnpCheckMain implements Callable<Integer> {
     public Integer call() {
         try {
             HmfApi hmfApi = HmfApi.create(apiUrl);
-            GoogleCredentials snpCheckCredentials = GoogleCredentials.fromStream(new FileInputStream(snpcheckPrivateKeyPath));
-            GoogleCredentials databaseCredentials = GoogleCredentials.fromStream(new FileInputStream(databasePrivateKeyPath));
-            Bucket snpcheckBucket =
-                    StorageOptions.newBuilder().setCredentials(snpCheckCredentials).build().getService().get(snpcheckBucketName);
+            Bucket snpcheckBucket = StorageOptions.getDefaultInstance().getService().get(snpcheckBucketName);
             if (snpcheckBucket == null) {
                 LOGGER.error("Bucket [{}] does not exist. ", snpcheckBucketName);
                 return 1;
             }
 
             EventSubscriber.create(project,
-                    snpCheckCredentials,
-                    PipelineComplete.subscription(project, "snpcheck", snpCheckCredentials),
+                    PipelineComplete.subscription(project, "snpcheck"),
                     PipelineComplete.class)
                     .subscribe(new SnpCheck(hmfApi.runs(),
                             hmfApi.samples(),
                             snpcheckBucket,
-                            StorageOptions.newBuilder().setCredentials(databaseCredentials).build().getService(),
+                            StorageOptions.getDefaultInstance().getService(),
                             new PerlVcfComparison(),
-                            Publisher.newBuilder(ProjectTopicName.of(project, "turquoise.events"))
-                                    .setCredentialsProvider(() -> snpCheckCredentials)
-                                    .build(),
-                            Publisher.newBuilder(ProjectTopicName.of(project, PipelineValidated.TOPIC))
-                                    .setCredentialsProvider(() -> snpCheckCredentials)
-                                    .build(),
+                            Publisher.newBuilder(ProjectTopicName.of(project, "turquoise.events")).build(),
+                            Publisher.newBuilder(ProjectTopicName.of(project, PipelineValidated.TOPIC)).build(),
                             OBJECT_MAPPER, passthru));
             return 0;
         } catch (Exception e) {
@@ -96,7 +81,6 @@ public class SnpCheckMain implements Callable<Integer> {
     }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new SnpCheckMain()).execute(args);
-        System.exit(exitCode);
+        System.exit(new CommandLine(new SnpCheckMain()).execute(args));
     }
 }
